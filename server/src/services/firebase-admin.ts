8b99187703c3f961c2a48c660f8e1c6069c30df2
Ajay Sehwal <110731336+ajaysehwal@users.noreply.github.com>
2024-10-05 20:00:34 +0530

    Delete server directory

--- server/src/services/firebase-admin.ts
+++
@@ -1,9 +0,0 @@
-import * as admin from "firebase-admin";
-const serviceAccount = require("../../firebase.json");
-if (!admin.apps.length) {
-  admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
-  });
-}
-
-export { admin };
